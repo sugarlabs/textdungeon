65fcb6be7d1d5642dfedded925cc0e7ea879a18b
Dimitris Mylonas | dmylonas <dimitris20cen@gmail.com>
2023-07-31 13:49:11 +0300

    Update engine's functionality
    - Add copyright
    - Use imperative mood for comments
    - Change handling of json to correct issues with outputs

--- game_engine.py
+++ game_engine.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2023  Dimitios Mylonas
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import json
 import sys
 import shutil
@@ -18,8 +34,9 @@ class Game:
         self.player = Player(self.data, self)
         self.current_room = Room(
             self.data["rooms"].get(self.data["currentRoom"]), self.data, self)
-        
         self.print(self.data['opening'])
+        start_room = (self.data["currentRoom"])
+        self.print(self.data['rooms'][start_room].get('description'))
         self.current_room.describe_room()
 
         self.command_palette = {
@@ -41,7 +58,7 @@ class Game:
     create one. Read copied file and load it's contents
     game_data"""
     def load_map(self, filename):
-        self.copy_filename = filename.replace('maps/', 'tmp_')
+        self.copy_filename = 'tmp_' + filename
         if os.path.exists(self.copy_filename):
             try:
                 with open(self.copy_filename, 'r') as f:
@@ -60,19 +77,16 @@ class Game:
             except json.JSONDecodeError:
                 sys.exit('Error decoding the map file.')
 
-    """This function writes changes in game data (game state)
-    back to the JSON file"""
+    """Write changes in game state back to the JSON file"""
     def modify_map(self, game_data):
         with open(self.copy_filename, 'w') as f:
             json.dump(game_data, f, indent=4)
 
-    """This function moves the player to a new room in the specified direction.
+    """Move player to a new room in the specified direction.
     If there's a required item to enter the room,
     the player must possess it to enter."""
     def move_player(self, direction):
-        new_room_name = self.current_room.move_player(direction)
-        self.data['currentRoom'] = new_room_name
-        self.modify_map(self.data)
+        new_room_name = self.current_room.check_deadend(direction)
         if new_room_name:
             new_room_data = self.data["rooms"].get(new_room_name)
             required_item = new_room_data.get("requiredItem")
@@ -82,15 +96,24 @@ class Game:
                     if required_item not in self.data.get("inventory"):
                         self.print(self.data['rooms'][new_room_name].get('failToEnter'))
                         return
-                    self.print(self.data['rooms'][new_room_name].get('failToEnter'))
+                    self.print(self.data['rooms'][new_room_name].get('entryMsg'))
+                    self.data['currentRoom'] = new_room_name
+                    self.current_room = Room(new_room_data, self.data, self)
+                    self.current_room.entering_room()
+                    self.npc_interactions(new_room_name)
+                    self.modify_map(self.data)
                     return
+                self.print(self.data['rooms'][new_room_name].get('failToEnter'))
+                return
 
             self.current_room = Room(new_room_data, self.data, self)
+            self.data['currentRoom'] = new_room_name
+            self.modify_map(self.data)
             self.current_room.entering_room()
 
             self.npc_interactions(new_room_name)
 
-    """This function defines the interactions with the enemies-npcs
+    """Define interactions with the enemies-npcs
     and the thieves-npcs in the room."""
     def npc_interactions(self, room_name):
         npc_type = self.data['rooms'][room_name].get('npcType')
@@ -98,14 +121,16 @@ class Game:
             if self.data['npcs'][npc_type].get('toDefeat') in self.data["inventory"]:
                 self.print(self.data['npcs'][npc_type].get('youWin'))
                 if npc_type == "enemies":
-                    self.data['rooms'][room_name].pop('npcType', None)
-                    self.data['rooms'][room_name]['description'] = self.data['rooms'][room_name].get('emptyDescription')
-                    self.data['rooms'][room_name].pop('warning', None)
+                    room = self.data['rooms'][room_name]
+                    room.pop('npcType', None)
+                    room['description'] = self.data['rooms'][room_name].get(
+                        'emptyDescription')
+                    room.pop('warning', None)
                     self.modify_map(self.data)
             else:
                 if npc_type == "enemies":
                     self.print(self.data['npcs'][npc_type].get('youLose'))
-                    self.print("Type \"restart\" to start new game." )
+                    self.print("Type \"restart\" to start new game.")
                     return 0
                 if npc_type == "thieves":
                     item_to_remove = self.data['npcs'][npc_type].get("desires")
@@ -118,7 +143,7 @@ class Game:
                         self.modify_map(self.data)
         return 1
 
-    """This function allows the player to examine their surroundings,
+    """Allow player to examine their surroundings,
     giving a description of what's in each direction."""
     def look_around(self):
         self.current_room.describe_room()
@@ -129,7 +154,7 @@ class Game:
                 if self.data['rooms'][room_name].get('warning'):
                     self.print(self.data['rooms'][room_name]['warning'])
 
-    """Prints to gtk.textview instead of console"""
+    """Print to gtk.textview instead of console"""
     def print(self, text):
         self.game_window.print_to_textview(text)
 
@@ -156,7 +181,7 @@ class Game:
             self.print(self.data['genericMsgs']['invalidCmd'])
 
         if self.data['currentRoom'] == self.data.get("exitRoom"):
-            self.print("You won!\nType \"restart\" to start new game." )
+            self.print("You won!\nType \"restart\" to start new game.")
 
     def print_help(self):
         self.print(self.data['genericMsgs']['helpCmd'])
@@ -170,13 +195,14 @@ class Game:
         self.data = self.load_map(self.map_file)
         self.player.update_data(self.data)
         start_room_name = self.data.get("startRoom")
-        self.current_room = Room(self.data["rooms"].get(start_room_name), self.data, self)
+        self.current_room = Room(
+            self.data["rooms"].get(start_room_name), self.data, self)
         self.game_window.clear_textview()
         self.print("Game has successfully been restarted")
         self.current_room.describe_room()
 
     """
-    Goes to menu without deleting self.copy_filename
+    Go to menu without deleting self.copy_filename
     so no progress is lost
     """
     def go_to_menu(self):
@@ -197,7 +223,6 @@ class Room:
         self.data = data
         self.game = game
 
-    """This function is called when player enters a room"""
     def entering_room(self):
         if self.room_data.get('entryMsg'):
             self.game.print(self.room_data['entryMsg'])
@@ -205,19 +230,20 @@ class Room:
         if self.room_data.get('items'):
             self.game.print('You see: ' + ', '.join(self.room_data['items']))
 
-    """This function is called to describe room and items in it"""
+    """Describe room and items in it"""
     def describe_room(self):
-        self.game.print(self.room_data['description'])
         if self.room_data.get('npcType'):
-            npc_description = self.data['npcs'][self.room_data['npcType']].get('description')
+            npc_description = (
+                self.data['npcs'][self.room_data['npcType']].get('description')
+            )
             if npc_description:
                 self.game.print(npc_description)
         if self.room_data.get('items'):
             self.game.print('You see: ' + ', '.join(self.room_data['items']))
 
-    """This function checks if a move in the given direction is possible.
+    """Check if a move in the given direction is possible.
     If not, it informs the player they've hit a dead end."""
-    def move_player(self, direction):
+    def check_deadend(self, direction):
         if direction in self.room_data:
             return self.room_data[direction]
         else:
@@ -248,7 +274,7 @@ class Player:
         else:
             self.game.print(self.data['genericMsgs']['noItemHere'])
 
-    """This function allows the player to use an item from their inventory.
+    """Allow player to use an item from their inventory.
     If the room has a trader NPC,
     the player can trade with the NPC."""
     def use_item(self, item, current_room):
@@ -261,9 +287,11 @@ class Player:
                     sells_item = self.data['npcs'][npc_type]['sells']
                     self.data['inventory'].append(sells_item)
                     self.game.modify_map(self.data)
-                    self.game.print(f"You successfully traded your {item} with the {sells_item}!")
+                    self.game.print(
+                        f"You successfully traded your {item} with the {sells_item}!")
             else:
-                self.game.print(f"You can't use your {item} here, try in a different room!")
+                self.game.print(
+                    f"You can't use your {item} here, try in a different room!")
         else:
             self.game.print('You do not have such item in your inventory.')
 
